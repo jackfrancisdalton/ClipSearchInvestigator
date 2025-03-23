5636d80d33646d1452d263e15adfacc949ffe975
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-03-23 17:23:33 +0000

    feat: added base tests

--- backend/app/video_fetcher.py
+++ backend/app/video_fetcher.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 
 from app.pydantic_schemas.search_results import YoutubeVideoData
 from app.exceptions.shared import ChannelSearchError, VideoSearchError
-import json
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
 load_dotenv(dotenv_path=dotenv_path)
