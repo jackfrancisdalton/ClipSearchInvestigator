ba950a3df232ae98f4ab33b16d488a83c3a38ec3
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-05-30 21:32:26 +0100

    refactor: cleaned up video service tests

--- backend/tests/services/test_video_service.py
+++ backend/tests/services/test_video_service.py
@@ -33,7 +33,32 @@ def mock_response() -> Dict[str, List[Dict[str, Any]]]: # TODO: sort out the typ
     }
 
 @patch('app.services.video_service.requests.get')
-def test_search_youtube_success(mock_get: Mock, mock_response: Dict[str, List[Dict[str, Any]]]):
+def test_search_youtube__no_results(mock_get: Mock):
+    mock_get.return_value = Mock(status_code=200)
+    mock_get.return_value.json.return_value = {"items": []}
+
+    api_key = "test_api_key"
+    video_search_query = "test query"
+    sort_order = "date"
+    published_before = date(2023, 1, 3)
+    published_after = date(2023, 1, 1)
+    channel_name = None
+    max_results = 10
+
+    results = search_youtube(
+        api_key=api_key,
+        video_search_query=video_search_query,
+        sort_order=sort_order,
+        published_before=published_before,
+        published_after=published_after,
+        channel_name=channel_name,
+        max_results=max_results
+    )
+
+    assert len(results) == 0
+
+@patch('app.services.video_service.requests.get')
+def test_search_youtube__results_found(mock_get: Mock, mock_response: Dict[str, List[Dict[str, Any]]]):
     mock_get.return_value = Mock(status_code=200)
     mock_get.return_value.json.return_value = mock_response
 
@@ -71,7 +96,7 @@ def test_search_youtube_success(mock_get: Mock, mock_response: Dict[str, List[Di
     assert results[1].thumbnailUrl == "http://example.com/thumbnail2.jpg"
 
 @patch('app.services.video_service.requests.get')
-def test_search_youtube_failure(mock_get: Mock):
+def test_search_youtube__exception_handling(mock_get: Mock):
     mock_get.return_value = Mock(status_code=500)
     mock_get.return_value.raise_for_status.side_effect = Exception("API call failed")
 
