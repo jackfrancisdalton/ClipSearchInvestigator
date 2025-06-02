5b67a1394b74c73dc1cfa5267251cf630748fafc
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-06-02 15:20:06 +0100

    test: added test for factory reset component

--- frontend/src/components/Options/FactoryResetApp.tsx
+++ frontend/src/components/Options/FactoryResetApp.tsx
@@ -8,13 +8,23 @@ interface DeleteApiKeysButtonProps {
 const FactoryResetApp: React.FC<DeleteApiKeysButtonProps> = ({ onDeleteAll, errorMessage }) => {
   return (
     <div className="mt-4">
+
       <button
-        className="px-4 py-2 font-medium bg-red-600 rounded text-white"
+        className="px-4 py-2 font-medium text-white bg-red-600 rounded"
         onClick={onDeleteAll}
+        data-testid="factory-reset-app_button"
       >
         Factory Reset App
       </button>
-      {errorMessage && <div className="mt-1 text-sm text-red-500">{errorMessage}</div>}
+
+      {errorMessage && 
+        <div 
+          className="mt-1 text-sm text-red-500"
+          data-testid="factory-reset-app_error"
+        >
+          {errorMessage}
+        </div>
+      }
     </div>
   );
 };
